0b9a49896c0a053ac9d291c5166221bec69cefc9
Thunderoar <farhansamilme@gmail.com>
2024-09-29 23:50:46 +0800

    establish an undeletable core plan 'MRBDPX'(obselete)

--- dashboard/admin/del_plan.php
+++ dashboard/admin/del_plan.php
@@ -3,9 +3,13 @@ require '../../include/db_conn.php';
 page_protect();
 
 $msgid = $_POST['name'];
-if (strlen($msgid) > 0) {
+
+if ($msgid === 'MRBDPX') {
+    echo "<html><head><script>alert('You cannot delete a core plan');</script></head></html>";
+    echo "<meta http-equiv='refresh' content='0; url=view_plan.php'>";
+} elseif (strlen($msgid) > 0) {
     mysqli_query($con, "update plan set active ='no' WHERE pid='$msgid'");
-    echo "<html><head><script>alert('Member Deleted');</script></head></html>";
+    echo "<html><head><script>alert('Plan Deleted');</script></head></html>";
     echo "<meta http-equiv='refresh' content='0; url=view_plan.php'>";
 } else {
     echo "<html><head><script>alert('ERROR! Delete Opertaion Unsucessfull');</script></head></html>";
