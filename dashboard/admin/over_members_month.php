c39c8755e456f38f505cebb7f791ca108fe4e6f0
Rokyugen <farhansamilme2@gmail.com>
2024-09-30 21:07:24 +0800

    over_members_month.php

--- dashboard/admin/over_members_month.php
+++ dashboard/admin/over_members_month.php
@@ -15,9 +15,9 @@ page_protect();
     <link rel="stylesheet" href="../../css/dashMain.css">
     <link rel="stylesheet" type="text/css" href="../../css/entypo.css">
 	<link href="a1style.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="../../css/bootstrap.min.css">
+	<script src="../../js/jquery.min.js"></script>
+	<script src="../../js/bootstrap.min.js"></script>
      <style>
     	.page-container .sidebar-menu #main-menu li#overviewhassubopen > a {
     	background-color: #2b303a;
