35d089778e83ff9ae9c88eed69de979f0f4af976
Rokyugen <farhansamilme2@gmail.com>
2024-10-02 15:38:46 +0800

    update database

--- karate_club_db.sql
+++ karate_club_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 29, 2024 at 05:54 PM
+-- Generation Time: Oct 02, 2024 at 09:37 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,8 @@ CREATE TABLE `address` (
 
 INSERT INTO `address` (`addressid`, `userid`, `streetName`, `state`, `city`, `zipcode`) VALUES
 ('426746007', '1727621388', 'abc', 'abc', 'abc', ''),
-('395689128', '1727621728', 'Kampung Lago Peti Surat 240', 'SABAH', 'SABAH - BEAUFORT/DL MAMPAKUR', '');
+('395689128', '1727621728', 'Kampung Lago Peti Surat 240', 'SABAH', 'SABAH - BEAUFORT/DL MAMPAKUR', ''),
+('126880847', '1727665527', 'johanstreet', 'johanstate', 'johancity', '');
 
 -- --------------------------------------------------------
 
@@ -90,7 +91,8 @@ INSERT INTO `enrolls_to` (`et_id`, `planid`, `userid`, `paid_date`, `expire`, `r
 ('2024-09-29 22:48:36', 'BJEFSY', '1727621310', '2024-09-29', '1970-01-01', 'yes'),
 ('2024-09-29 22:49:50', 'BJEFSY', '1727621388', '2024-09-29', '1970-01-01', 'yes'),
 ('2024-09-29 22:53:44', 'BJEFSY', '1727621622', '2024-09-29', '1970-01-01', 'yes'),
-('2024-09-29 22:55:31', 'BJEFSY', '1727621728', '2024-09-29', '1970-01-01', 'yes');
+('2024-09-29 22:55:31', 'BJEFSY', '1727621728', '2024-09-29', '1970-01-01', 'yes'),
+('2024-09-30 11:06:25', 'BJEFSY', '1727665527', '2024-09-30', '1970-01-01', 'yes');
 
 -- --------------------------------------------------------
 
@@ -115,7 +117,29 @@ CREATE TABLE `health_status` (
 INSERT INTO `health_status` (`healthid`, `calorie`, `height`, `weight`, `fat`, `remarks`, `userid`) VALUES
 ('689152795', '', '', '', '', '', '1727621388'),
 ('947924960', '', '', '', '', '', '1727621622'),
-('327687540', '', '', '', '', '', '1727621728');
+('327687540', '', '', '', '', '', '1727621728'),
+('255603186', '', '', '', '', 'WOW!', '1727665527');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(20) NOT NULL,
+  `pass_key` varchar(20) NOT NULL,
+  `securekey` varchar(20) NOT NULL,
+  `authority` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `pass_key`, `securekey`, `authority`) VALUES
+('test', 'test', 'test', 'member'),
+('admintest', 'admintest', 'admintest', 'admin');
 
 -- --------------------------------------------------------
 
@@ -140,8 +164,9 @@ CREATE TABLE `plan` (
   `planid` varchar(8) NOT NULL,
   `planName` varchar(20) NOT NULL,
   `description` varchar(200) NOT NULL,
+  `planType` varchar(50) NOT NULL,
   `validity` varchar(20) NOT NULL DEFAULT 'Lifetime',
-  `amount` int(10) NOT NULL,
+  `amount` int(10) NOT NULL DEFAULT 0,
   `active` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -149,8 +174,13 @@ CREATE TABLE `plan` (
 -- Dumping data for table `plan`
 --
 
-INSERT INTO `plan` (`planid`, `planName`, `description`, `validity`, `amount`, `active`) VALUES
-('BJEFSY', 'Karate Activities', 'This includes all karate activity plan', 'Lifetime', 20, 'yes');
+INSERT INTO `plan` (`planid`, `planName`, `description`, `planType`, `validity`, `amount`, `active`) VALUES
+('BJEFSY', 'Karate Activities', 'This includes all karate activity plan', 'Core', 'Lifetime', 20, 'yes'),
+('IBJAGF', 'Fighting!', 'fight', 'Event', 'Lifetime', 0, 'no'),
+('LNSZPE', 'Fighting!', 'fight', 'Collaboration', 'Lifetime', 0, 'no'),
+('MELZXV', 'Fighting!', 'fight', 'Collaboration', 'Lifetime', 0, 'no'),
+('SDKTPH', 'Important Event', 'WOW!', 'Event', 'Lifetime', 0, 'yes'),
+('XZKYQE', 'Fighting!', 'fight', '', 'Lifetime', 0, 'no');
 
 -- --------------------------------------------------------
 
@@ -196,7 +226,18 @@ INSERT INTO `users` (`userid`, `username`, `gender`, `mobile`, `email`, `dob`, `
 ('1727618166', 'abc', 'Male', '0132017340', 'fa@gmail.com', '2020-02-19', '2024-05-20'),
 ('1727618190', 'abc', 'Male', '0132017340', 'fa@gmail.com', '2020-02-19', '2024-05-20'),
 ('1727621287', 'abc', 'Male', '0132017340', 'fa@gmail.com', '2020-10-10', '2024-02-01'),
-('1727621728', 'Muhammad Farhan bin Samil', 'Male', '0196340894', 'farhansamilme@gmail.com', '2004-12-28', '2024-09-10');
+('1727621728', 'Muhammad Farhan bin Samil', 'Male', '0196340894', 'farhansamilme@gmail.com', '2004-12-28', '2024-09-10'),
+('1727665527', 'johan', 'Male', '019', 'johan@gmail.com', '2020-09-07', '2024-09-08');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `plan`
+--
+ALTER TABLE `plan`
+  ADD PRIMARY KEY (`planid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
