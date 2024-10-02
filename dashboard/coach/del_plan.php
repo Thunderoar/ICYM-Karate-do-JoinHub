e4c09e8ba870aeca8dca8d392e710699e0553a50
Rokyugen <farhansamilme2@gmail.com>
2024-10-02 13:59:59 +0800

    add coach section

---
+++ dashboard/coach/del_plan.php
@@ -0,0 +1,19 @@
+<?php
+require '../../include/db_conn.php';
+page_protect();
+
+$msgid = $_POST['name'];
+
+if ($msgid === 'BJEFSY') {
+    echo "<html><head><script>alert('You cannot delete a core plan');</script></head></html>";
+    echo "<meta http-equiv='refresh' content='0; url=view_plan.php'>";
+} elseif (strlen($msgid) > 0) {
+    mysqli_query($con, "update plan set active ='no' WHERE pid='$msgid'");
+    echo "<html><head><script>alert('Plan Deleted');</script></head></html>";
+    echo "<meta http-equiv='refresh' content='0; url=view_plan.php'>";
+} else {
+    echo "<html><head><script>alert('ERROR! Delete Opertaion Unsucessfull');</script></head></html>";
+   echo "error".mysqli_error($con);
+}
+
+?>
---
+++ dashboard/coach/plandetail.php
@@ -0,0 +1,19 @@
+<?php
+require '../../include/db_conn.php';
+$planid=$_GET['q'];
+$query="select * from plan where planid='".$planid."'";
+$res=mysqli_query($con,$query);
+if($res){
+	$row=mysqli_fetch_array($res,MYSQLI_ASSOC);
+	// echo "<tr><td>".$row['amount']."</td></tr>";
+	echo "<tr>
+		<td height='35'>AMOUNT:</td>
+		<td height='35'>&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input id='boxx' type='text' value='RM".$row['amount']."' readonly></td></tr>
+		<tr>
+		<td height='35'>VALIDITY:</td>
+		<td height='35'>&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input type='text' id='boxx' value='".$row['validity']."' readonly></td>
+		</tr>
+	";
+}
+
+?>
