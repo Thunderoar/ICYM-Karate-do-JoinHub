fc8f48086ba99a3126a514211c79d8890830c394
Rokyugen <farhansamilme2@gmail.com>
2024-10-06 14:06:25 +0800

    seprate gallery section into its own file in /element/ for easier editing

--- gallery.php
+++ gallery.php
@@ -56,43 +56,9 @@ require('header.php');
     </div>
 
     
-    <div class="site-section">
-      <div class="container">
-        <div class="row align-items-center mb-2">
-          <div class="col-6">
-            <h2 class="section-title">Team Gallery</h2>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/image_8.jpg" data-fancybox="gal"><img src="images/image_8.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/image_2.jpg" data-fancybox="gal"><img src="images/image_2.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/image_3.jpg" data-fancybox="gal"><img src="images/image_3.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/image_6.jpg" data-fancybox="gal"><img src="images/image_6.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/image_12.jpg" data-fancybox="gal"><img src="images/image_12.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/image_11.jpg" data-fancybox="gal"><img src="images/image_11.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/event_2.jpg" data-fancybox="gal"><img src="images/event_2.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="images/image_10.jpg" data-fancybox="gal"><img src="images/image_10.jpg" alt="Image" class="img-fluid"></a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php
+    require('element/gallery.php');
+    ?>
 
 <?php
 require('footer.html');
