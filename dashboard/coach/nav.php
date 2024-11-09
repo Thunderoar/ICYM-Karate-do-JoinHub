f4b22bef6be7a63715150667e23a5d421df36f7f
Rokyugen <farhansamilme@gmail.com>
2024-11-09 20:56:34 +0800

    nav.php(coach): remove 'Timetable', Add 'Event Planning'

--- dashboard/coach/nav.php
+++ dashboard/coach/nav.php
@@ -3,14 +3,16 @@
 <ul id="main-menu">
         <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>
         <li id="paymnt"><a href="payments.php"><i class="entypo-star"></i><span>Payments</span></a></li>
-        <li id="health_status"><a href="new_health_status.php"><i class="entypo-user-add"></i><span>Health Status</span></a></li>
-        <li id="planhassubopen">
+        <!--<li id="health_status"><a href="new_health_status.php"><i class="entypo-user-add"></i><span>Health Status</span></a></li> -->
+				<li><a href="view_plan.php"><i class="entypo-quote"></i><span>Event Planning</span></a></li>
+        <!--<li id="planhassubopen">
             <a href="#" onclick="memberExpand(2)"><i class="entypo-quote"></i><span>Planning</span></a>
             <ul id="planExpand">
                 <li><a href="view_plan.php"><span>Activity list</span></a></li>
             </ul>
-        </li>
-	<li class="" id="routinehassubopen"><a href="#" onclick="memberExpand(4)"><i class="entypo-alert"></i><span>Timetable</span></a>
+        </li> -->
+	<li><a href="view_mem.php"><i class="entypo-users"></i><span>Your Student</span></a></li>
+	<!--<li class="" id="routinehassubopen"><a href="#" onclick="memberExpand(4)"><i class="entypo-alert"></i><span>Timetable</span></a>
 
 		<ul id="routineExpand">
 			<li class="active">
@@ -27,7 +29,7 @@
 
 		</ul>
 
-	</li>
+	</li> -->
         <li id="adminprofile"><a href="more-userprofile.php"><i class="entypo-folder"></i><span>Profile</span></a></li>
         <li><a href="logout.php"><i class="entypo-logout"></i><span>Logout</span></a></li>
     </ul>
