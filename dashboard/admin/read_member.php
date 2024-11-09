f1cc85a55c1c99906e25c1da1a847de38a60d568
Rokyugen <farhansamilme@gmail.com>
2024-11-09 20:53:20 +0800

    read_member.php: return button go to right page

--- dashboard/admin/read_member.php
+++ dashboard/admin/read_member.php
@@ -225,7 +225,11 @@ line-height:0.5cm;
 
 
 			<?php include('footer.php'); ?>
-			<a class="btn-sm px-4 py-3 d-flex home-button" style="background-color:#2a2e32" href="view_mem.php">Return Back</a>	
+<a class="btn-sm px-4 py-3 d-flex home-button" style="background-color:#2a2e32" 
+   href="<?php echo (isset($_SESSION['is_admin_logged_in']) && $_SESSION['is_admin_logged_in'] ? 'view_mem.php' : '../../dashboard/coach/view_mem.php'); ?>">
+    Return Back
+</a>
+
     	</div>
     </body>
 </html>
