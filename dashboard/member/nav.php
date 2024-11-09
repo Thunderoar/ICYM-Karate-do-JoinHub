4174bf804d8e369f9e48e4abd97006ee0205862d
Rokyugen <farhansamilme@gmail.com>
2024-11-09 20:59:09 +0800

    nav.php(member): remove 'Timetable', add 'Event Planning'

--- dashboard/member/nav.php
+++ dashboard/member/nav.php
@@ -22,18 +22,20 @@ $isApproved = $row['hasApproved'];
         <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>
         <li id="paymnt"><a href="payments.php"><i class="entypo-star"></i><span>Payments</span></a></li>
         <li id="health_status"><a href="new_health_status.php"><i class="entypo-user-add"></i><span>Health Status</span></a></li>
-        <li id="planhassubopen">
+		<li><a href="view_plan.php"><i class="entypo-quote"></i><span>Event Planning</span></a></li>
+        <!-- <li id="planhassubopen">
             <a href="#" onclick="memberExpand(2)"><i class="entypo-quote"></i><span>Planning</span></a>
             <ul id="planExpand">
-                <li><a href="view_plan.php"><span>Activity list</span></a></li>
+
             </ul>
-        </li>
-        <li id="routinehassubopen">
+        </li> -->
+		<!--<li><a href="viewroutine.php"><i class="entypo-alert"></i><span>View Timetable</span></a></li>
+         <li id="routinehassubopen">
             <a href="#" onclick="memberExpand(4)"><i class="entypo-alert"></i><span>Timetable</span></a>
             <ul id="routineExpand">
-                <li><a href="viewroutine.php"><span>View Timetable</span></a></li>
+
             </ul>
-        </li>
+        </li> -->
         <li id="adminprofile"><a href="more-userprofile.php"><i class="entypo-folder"></i><span>Profile</span></a></li>
         <li><a href="logout.php"><i class="entypo-logout"></i><span>Logout</span></a></li>
     <?php endif; ?>
