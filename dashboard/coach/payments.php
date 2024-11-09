9d56ec0da9fcab4e30ec7a40a7a1b78bea4fa44a
Rokyugen <farhansamilme@gmail.com>
2024-11-09 20:56:51 +0800

    payments.php(coach): same payment page like admin

--- dashboard/coach/payments.php
+++ dashboard/coach/payments.php
@@ -1,143 +1,221 @@
 ﻿<?php
 require '../../include/db_conn.php';
 page_protect();
-
-if (!isset($_SESSION['staffid'])) {
-    // Redirect to login page or display an error if the user is not logged in
-    header("Location: login.php");
-    exit();
-}
-
-$current_user_id = $_SESSION['staffid'];
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>ICYM Karate-Do | Payments</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../css/style.css" id="style-resource-5">
+
+    <title>ICYM Karate-Do  | Payments</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../../css/style.css"  id="style-resource-5">
     <script type="text/javascript" src="../../js/Script.js"></script>
     <link rel="stylesheet" href="../../css/dashMain.css">
     <link rel="stylesheet" type="text/css" href="../../css/entypo.css">
     <link href="a1style.css" type="text/css" rel="stylesheet">
-    <link rel="stylesheet" href="../../css/bootstrap.min.css">
-    <script src="../../js/jquery.min.js"></script>
-    <script src="../../js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="../../css/dashboard/sidebar.css"> 
+	<link rel="stylesheet" href="../../css/bootstrap.min.css">
+	<script src="../../js/jquery.min.js"></script>
+	<script src="../../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../../css/insidedashboard.css">
+	
+	<link rel="stylesheet" href="../../css/dashboard/sidebar.css">
     <style>
-        .page-container .sidebar-menu #main-menu li#paymnt > a {
-            background-color: #2b303a;
-            color: #ffffff;
-        }
+    	.page-container .sidebar-menu #main-menu li#paymnt > a {
+    	background-color: #2b303a;
+    	color: #ffffff;
+		}
+
     </style>
+
 </head>
-<body class="page-body page-fade" onload="collapseSidebar()">
-    <div class="page-container sidebar-collapsed" id="navbarcollapse">	
-        <div class="sidebar-menu">
-            <header class="logo-env">
-                <!-- logo -->
-                <?php require('../../element/loggedin-logo.html'); ?>
-                <!-- logo collapse icon -->
-                <div class="sidebar-collapse" onclick="collapseSidebar()">
-                    <a href="#" class="sidebar-collapse-icon with-animation">
-                        <i class="entypo-menu"></i>
-                    </a>
-                </div>
-            </header>
-            <?php include('nav.php'); ?>
-        </div>
+      <body class="page-body  page-fade" onload="collapseSidebar()">
 
-        <div class="main-content">
-            <div class="row">
-                <!-- Profile Info and Notifications -->
-                <div class="col-md-6 col-sm-8 clearfix"></div>
+    	<div class="page-container sidebar-collapsed" id="navbarcollapse">	
+	
+		<div class="sidebar-menu">
+	
+			<header class="logo-env">
+			
+			<!-- logo -->
+			<?php
+			 require('../../element/loggedin-logo.html');
+			?>
+			
+					<!-- logo collapse icon -->
+					<div class="sidebar-collapse" onclick="collapseSidebar()">
+				<a href="#" class="sidebar-collapse-icon with-animation"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
+					<i class="entypo-menu"></i>
+				</a>
+			</div>
+							
+			
+		
+			</header>
+    		<?php include('nav.php'); ?>
+    	</div>
 
-                <!-- Raw Links -->
-                <div class="col-md-6 col-sm-4 clearfix hidden-xs">
-                    <ul class="list-inline links-list pull-right">
-					<?php
+    		<div class="main-content">
+		
+				<div class="row">
+					
+					<!-- Profile Info and Notifications -->
+					<div class="col-md-6 col-sm-8 clearfix">	
+							
+					</div>
+					
+					
+					<!-- Raw Links -->
+					<div class="col-md-6 col-sm-4 clearfix hidden-xs">
+						
+						<ul class="list-inline links-list pull-right">
+
+                        <?php
 						require('../../element/loggedin-welcome.html');
-					?>							
-                        <li>
-                            <a href="logout.php">Log Out <i class="entypo-logout right"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+					?>
+							</li>								
+						
+							<li>
+								<a href="logout.php">
+									Log Out <i class="entypo-logout right"></i>
+								</a>
+							</li>
+						</ul>
+						
+					</div>
+					
+				</div>
 
-            <h2>Payments</h2>
-            <hr />
+				<h2>Payments</h2>
+<hr />
 
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Sl.No</th>
-                        <!-- <th>Membership Expiry</th> -->
-                        <th>Name</th>
-                        <th>Member ID</th>
-                        <th>Phone</th>
-                        <th>E-Mail</th>
-                        <th>Gender</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-// Update query to show only the current user's enrollments
-$query  = "SELECT * FROM enrolls_to WHERE userid = '$current_user_id' ORDER BY expire";
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Sl.No</th>
+            <th>Member ID</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>E-Mail</th>
+            <th>Plan Name</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php
+$query = "SELECT e.*, u.username, u.mobile, u.email, p.planName 
+          FROM enrolls_to e
+          JOIN users u ON e.userid = u.userid
+          JOIN plan p ON e.planid = p.planid
+          ORDER BY e.expire";
 $result = mysqli_query($con, $query);
-$sno    = 1;
+$sno = 1;
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         $uid = $row['userid'];
         $planid = $row['planid'];
-        $hasPaid = $row['hasPaid']; // Check if the user has already paid
+        $planName = $row['planName'];
+        $hasPaid = $row['hasPaid'];
+        $hasApproved = $row['hasApproved'];
+        $et_id = $row['et_id'];
+        $receiptIMG = $row['receiptIMG'];
 
-        // Debugging: Output the hasPaid value
-        echo "<!-- Debug: User ID: $uid, hasPaid: $hasPaid -->"; 
+        echo "<tr>";
+        echo "<td>" . $sno . "</td>";
+        echo "<td>" . htmlspecialchars($row['userid']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['username']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['mobile']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['email']) . "</td>";
+        echo "<td>" . htmlspecialchars($planName) . "</td>"; 
 
-        $query1 = "SELECT * FROM users WHERE userid='$uid'";
-        $result1 = mysqli_query($con, $query1);
-        if (mysqli_num_rows($result1) == 1) {
-            while ($row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC)) {
-                echo "<tr><td>" . $sno . "</td>";
-                //echo "<td>" . $row['expire'] . "</td>";
-                echo "<td>" . $row1['username'] . "</td>";
-                echo "<td>" . $row1['userid'] . "</td>";
-                echo "<td>" . $row1['mobile'] . "</td>";
-                echo "<td>" . $row1['email'] . "</td>";
-                echo "<td>" . $row1['gender'] . "</td>";
+        $sno++;
 
-                $sno++;
-
-                // Only show "Add Payment" button if user has not paid
-                if ($hasPaid === 'no') { // Use strict comparison
-                    echo "<td>
-                            <form action='make_payments.php' method='post'>
-                                <input type='hidden' name='userID' value='" . $uid . "'/>
-                                <input type='hidden' name='planID' value='" . $planid . "'/>
-                                <input type='submit' class='a1-btn a1-blue' value='Add Payment' class='btn btn-info'/>
-                            </form>
-                          </td>";
-                } else {
-                    echo "<td>No Action Needed</td>"; // Indicate no action is needed
-                }
+        // Action based on payment and approval status
+        if ($hasPaid === 'no') {
+            // Payment not made
+            echo "<td>
+                    <form action='make_payments.php' method='post'>
+                        <input type='hidden' name='userID' value='" . htmlspecialchars($uid) . "'/>
+                        <input type='hidden' name='planID' value='" . htmlspecialchars($planid) . "'/>
+                        <input type='hidden' name='et_id' value='" . htmlspecialchars($et_id) . "'/>
+                        <input type='submit' class='a1-btn a1-blue' value='Confirm Payment'/>
+                    </form>
+                    <p>Payment ID: $et_id</p>
+                  </td>";
+        } elseif ($hasPaid === 'yes' && $hasApproved === 'no') {
+            // Payment made but not approved
+            echo "<td>
+                    <form action='make_payments.php' method='post' style='display:inline;'>
+                        <input type='hidden' name='userID' value='" . htmlspecialchars($uid) . "'/>
+                        <input type='hidden' name='planID' value='" . htmlspecialchars($planid) . "'/>
+                        <input type='hidden' name='et_id' value='" . htmlspecialchars($et_id) . "'/>
+                        <input type='submit' class='a1-btn a1-green' value='Approve Payment'/>
+                    </form>
+                    <form action='cancel_payment.php' method='post' style='display:inline;'>
+                        <input type='hidden' name='et_id' value='" . htmlspecialchars($et_id) . "'/>
+                        <input type='hidden' name='userID' value='" . htmlspecialchars($uid) . "'/>
+                        <input type='submit' class='a1-btn a1-yellow' value='Cancel Payment' onclick='return confirm(\"Are you sure you want to cancel this payment?\");'/>
+                    </form>";
+					
+			 // Check if receipt is available
+            if (!empty($receiptIMG)) {
+                echo "<a href='$receiptIMG' class='a1-btn a1-orange' target='_blank'>View Receipt</a>";
+            } else {
+                echo "<p>No Receipt Available</p>";
             }
+					
+			echo"<p>Payment Pending Approval</p>
+                    <p>Payment ID: $et_id</p>
+                  </td>";
+        } elseif ($hasPaid === 'yes' && $hasApproved === 'yes') {
+            // Payment made and approved
+            echo "<td>
+                    <p>Payment Approved</p>
+                    <p>Payment ID: $et_id</p>";
+
+			// Check if receipt is available
+            if (!empty($receiptIMG)) {
+                echo "<a href='$receiptIMG' class='a1-btn a1-orange' target='_blank'>View Receipt</a>";
+            } else {
+                echo "<p>No Receipt Available</p>";
+            }
+
+            // Button to undo approved payment
+            echo "<form action='undo_payment.php' method='post' style='display:inline;'>
+                    <input type='hidden' name='et_id' value='" . htmlspecialchars($et_id) . "'/>
+                    <input type='hidden' name='userID' value='" . htmlspecialchars($uid) . "'/>
+                    <input type='hidden' name='planID' value='" . htmlspecialchars($planid) . "'/>
+                    <input type='submit' class='a1-btn a1-red' value='Undo Approved Payment' onclick='return confirm(\"Are you sure you want to undo this payment?\");'/>
+                  </form>";
+
+            echo "</td>";
+        } else {
+            // Default fallback if neither condition applies
+            echo "<td>No Action Needed</td>";
         }
+        echo "</tr>";
     }
 } else {
-    echo "<tr><td colspan='8'>No payment records found.</td></tr>"; // Message for no records
+    echo "<tr><td colspan='7'>No records found</td></tr>";
 }
 ?>
+    </tbody>
+</table>
 
-                </tbody>
-            </table>
 
-            <?php include('footer.php'); ?>
-        </div>
+
+
+
+
+
+			<?php include('footer.php'); ?>
+    	</div>
+
     </body>
 </html>
-<!-- comment -->
+
+
