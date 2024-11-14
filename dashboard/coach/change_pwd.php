601f2f99301108b0e9300f19d1dcf23114f8440a
Rokyugen <farhansamilme@gmail.com>
2024-11-15 01:35:33 +0800

    change_pwd.php, change_s_pwd.php(coach): actually can change password

--- dashboard/coach/change_pwd.php
+++ dashboard/coach/change_pwd.php
@@ -14,6 +14,8 @@ page_protect();
     <link rel="stylesheet" href="../../css/dashMain.css">
     <link rel="stylesheet" type="text/css" href="../../css/entypo.css">
 	<link href="a1style.css" rel="stylesheet" type="text/css">
+	
+	<link rel="stylesheet" href="../../css/dashboard/sidebar.css"> 
 	<style>
     	.page-container .sidebar-menu #main-menu li#adminprofile > a {
     	background-color: #2b303a;
@@ -36,11 +38,9 @@ page_protect();
 			<header class="logo-env">
 			
 			<!-- logo -->
-			<div class="logo">
-				<a href="main.php">
-					<img src="logo1.png" alt="" width="192" height="80" />
-				</a>
-			</div>
+			<?php
+			 require('../../element/loggedin-logo.html');
+			?>
 			
 					<!-- logo collapse icon -->
 					<div class="sidebar-collapse" onclick="collapseSidebar()">
@@ -70,8 +70,9 @@ page_protect();
 						
 						<ul class="list-inline links-list pull-right">
 
-							<li>Welcome <?php echo $_SESSION['full_name']; ?> 
-							</li>							
+					<?php
+						require('../../element/loggedin-welcome.html');
+					?>								
 						
 							<li>
 								<a href="logout.php">
@@ -93,47 +94,61 @@ page_protect();
 		
 		
 		
-			<div class="a1-container a1-small a1-padding-32" style="margin-top:2px; margin-bottom:2px;">
-        <div class="a1-card-8 a1-light-gray" style="width:500px; margin:0 auto;">
-		<div class="a1-container a1-dark-gray a1-center">
-        	<h6>CHANGE PASSWORD</h6>
+<div class="a1-container a1-small a1-padding-32" style="margin-top:2px; margin-bottom:2px;">
+    <div class="a1-card-8 a1-light-gray" style="width:500px; margin:0 auto;">
+        <div class="a1-container a1-dark-gray a1-center">
+            <h6>CHANGE PASSWORD</h6>
         </div>
-       <form id="form1" name="form1" action="change_s_pwd.php" enctype="multipart/form-data" method="POST" class="a1-container" >
-         <table width="100%" border="0" align="center">
-         <tr>
-           <td height="35"><table width="100%" border="0" align="center">
-           	 <tr>
-           	   <td height="35">ID:</td>
-           	   <td height="35"><input type="text" id="boxx" name="login_id" readonly value="<?php echo $_SESSION['user_data']; ?>"  required/></td>
-         	   </tr>
-			   
-			   <tr>
-               <td height="35">LOGIN KEY:</td>
-               <td height="35"><input type="text" id="boxx" name="login_key"  class="form-control"  data-rule-required="true" placeholder="Your secret key"></td>
-             </tr>
-			 <tr>
-           	   <td height="35">PASSWORD:</td>
-           	   <td height="35"><input type="text" name="pwfield" id="boxx" class="form-control"  data-rule-required="true" data-rule-minlength="6" placeholder="Your new passowrd"></td>
-         	   </tr>
-             
-             <tr>
-			  <tr>
-           	   <td height="35">CONFIRM PASSWORD:</td>
-           	   <td height="35"><input type="text" name="confirmfield" id="boxx" class="form-control"  data-rule-equalto="#pwfield" data-rule-required="true" data-rule-minlength="6" placeholder="Confirm Your new passowrd"></td>
-         	   </tr>
-             
-             <tr>
-             <tr>
-               <td height="35"></td>
-               <td height="35"><a href="change_pwd.php" class="a1-btn a1-blue">SUBMIT</a><!--<input class="a1-btn a1-blue" type="submit" name="submit" id="submit" value="SUBMIT" >-->
-                 <input class="a1-btn a1-blue" type="reset" name="reset" id="reset" value="Reset"></td>
-             </tr>
-           </table></td>
-         </tr>
-         </table>
-       </form>
+        <form id="form1" name="form1" action="change_s_pwd.php" method="POST" class="a1-container">
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="entypo-user"></i>
+                    </div>
+                    <input type="text" class="form-control" name="login_id" readonly value="<?php echo $_SESSION['user_data']; ?>" placeholder="Your Login ID" required />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="entypo-key"></i>
+                    </div>
+                    <input type="text" name="login_key" class="form-control" placeholder="Your secret key" required minlength="6">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="entypo-key"></i>
+                    </div>
+                    <input type="password" name="pwfield" id="pwfield" class="form-control" placeholder="Your new password" required minlength="6">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="entypo-key"></i>
+                    </div>
+                    <input type="password" name="confirmfield" id="confirmfield" class="form-control" placeholder="Confirm your new password" required minlength="6" data-rule-equalto="#pwfield">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" name="submit" class="btn btn-primary">
+                    Change Password
+                    <i class="entypo-login"></i>
+                </button>
+                <a href="more-userprofile.php">
+                    <button type="button" class="btn btn-primary">Cancel</button>
+                </a>
+            </div>
+        </form>
     </div>
-    </div>   
+</div>
+
 		
 		
 		
--- dashboard/coach/change_s_pwd.php
+++ dashboard/coach/change_s_pwd.php
@@ -1,45 +1,46 @@
 <?php
-// $a = $_SERVER['HTTP_REFERER'];
-
-// if (strpos($a, '/e-has/') !== false) {
-    
-// } else {
-//     header("Location: ./");
-// }
-
-?>
-<?php
-// include 'index.php';
 require '../../include/db_conn.php';
-$key          = rtrim($_POST['login_key']);
-$pass         = rtrim($_POST['pwfield']);
-$user_id_auth = rtrim($_POST['login_id']);
-$passconfirm= rtrim($_POST['confirmfield']);
-if($pass==$passconfirm){
-if (isset($user_id_auth) && isset($pass) && isset($key)) {
-    $sql    = "SELECT * FROM admin WHERE username='$user_id_auth'";
-    $result = mysqli_query($con, $sql);
-    $count  = mysqli_num_rows($result);
-    if ($count == 1) {
-        mysqli_query($con, "UPDATE admin SET pass_key='$pass',securekey='$key' WHERE username='$user_id_auth'");
-        echo "<html><head><script>alert('Profile Updated ,Login Again ');</script></head></html>";
-        echo "<meta http-equiv='refresh' content='0; url=logout.php'>";
-    } else {
-        echo "<html><head><script>alert('Change Unsuccessful');</script></head></html>";
-        echo "error".mysqli_error($con);
+
+$key = trim($_POST['login_key']);
+$password = trim($_POST['pwfield']);
+$user_id_auth = trim($_POST['login_id']);
+$password_confirm = trim($_POST['confirmfield']);
+
+if ($password === $password_confirm) {
+    if (!empty($user_id_auth) && !empty($password) && !empty($key)) {
+        $sql = "SELECT * FROM staff WHERE username='$user_id_auth'";
+        $result = mysqli_query($con, $sql);
         
+        if ($result && mysqli_num_rows($result) === 1) {
+            // Update staff table
+            $update_staff_sql = "UPDATE staff SET pass_key='$password' WHERE username='$user_id_auth'";
+            if (mysqli_query($con, $update_staff_sql)) {
+                // Update securekey in login table
+                $update_login_sql = "UPDATE login SET securekey='$key' WHERE username='$user_id_auth'";
+                if (mysqli_query($con, $update_login_sql)) {
+                    echo "<html><head><script>alert('Profile Updated');</script></head></html>";
+                    echo "<meta http-equiv='refresh' content='0; url=more-userprofile.php'>";
+                } else {
+                    echo "<html><head><script>alert('Failed to update secure key');</script></head></html>";
+                    echo "Error: " . mysqli_error($con);
+                }
+            } else {
+                echo "<html><head><script>alert('Failed to update profile');</script></head></html>";
+                echo "Error: " . mysqli_error($con);
+            }
+        } else {
+            echo "<html><head><script>alert('User not found');</script></head></html>";
+            echo "<meta http-equiv='refresh' content='0; url=change_pwd.php'>";
+        }
+    } else {
+        echo "<html><head><script>alert('Missing required fields');</script></head></html>";
     }
 } else {
-    echo "<html><head><script>alert('Change Unsuccessful');</script></head></html>";
-    echo "error".mysqli_error($con);
- 
-}
-}
-else{
     echo "<html><head><script>alert('Confirm Password Mismatch');</script></head></html>";
     echo "<meta http-equiv='refresh' content='0; url=change_pwd.php'>";
 }
 ?>
+
 <center>
-<img src="loading.gif">
+    <img src="loading.gif">
 </center>
