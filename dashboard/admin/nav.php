fd7de7b0ff5ba6bc07cfdd62cc6c6879c15ed8ed
Rokyugen <farhansamilme@gmail.com>
2024-11-15 01:34:16 +0800

    nav.php(admin): if in accessing admin page through coach login, restrict certain navigation

--- dashboard/admin/nav.php
+++ dashboard/admin/nav.php
@@ -1,77 +1,29 @@
+<?php
 
-<ul id="main-menu" class="" >
-			
-    <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>
-                
-	<li id="regis"><a href="new_entry.php"><i class="entypo-user-add"></i><span>New Registration</span></a>                
-				
-	<li id="paymnt"><a href="payments.php"><i class="entypo-star"></i><span>Payments</span></a></li>
+// Start the session if it hasn't been started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 
-			<li class="active">
-				<a href="view_mem.php"><i class="entypo-users"></i><span>Members</span></a></li>
-				
-	<!-- <li class="" id="hassubopen"><a href="#" onclick="memberExpand(1)"><i class="entypo-users"></i><span>Members</span></a>
-		<ul id="memExpand">
-			<li class="active">
-				<a href="view_mem.php"><span>Edit Members</span></a></li>
+<ul id="main-menu">
+    <?php if (isset($_SESSION['is_admin_logged_in']) && $_SESSION['is_admin_logged_in']) { ?>
+        <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>
+        <li id="regis"><a href="new_entry.php"><i class="entypo-user-add"></i><span>New Registration</span></a></li>
+        <li id="paymnt"><a href="payments.php"><i class="entypo-star"></i><span>Payments</span></a></li>
+        <li class="active"><a href="view_mem.php"><i class="entypo-users"></i><span>Members</span></a></li>
+        <li><a href="view_plan.php"><i class="entypo-quote"></i><span>Event Planning</span></a></li>
+        <li id="messageCenter"><a href="messageCenter.php"><i class="entypo-mail"></i><span>Message Center</span></a></li>
+        <li id="adminprofile"><a href="more-userprofile.php"><i class="entypo-folder"></i><span>Profile</span></a></li>
+        <li><a href="logout.php"><i class="entypo-logout"></i><span>Logout</span></a></li>
+    <?php } elseif (isset($_SESSION['is_coach_logged_in']) && $_SESSION['is_coach_logged_in']) { ?>
+        <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>
+        <li id="paymnt"><a href="payments.php"><i class="entypo-star"></i><span>Payments</span></a></li>
+        <li><a href="view_plan.php"><i class="entypo-quote"></i><span>Event Planning</span></a></li>
+        <li><a href="view_mem.php"><i class="entypo-users"></i><span>Your Student</span></a></li>
+        <li id="adminprofile"><a href="more-userprofile.php"><i class="entypo-folder"></i><span>Profile</span></a></li>
+        <li><a href="logout.php"><i class="entypo-logout"></i><span>Logout</span></a></li>
+    <?php } ?>
+</ul>
 
-			<li><a href="table_view.php"><span>View Member</span></a></li>
-		</ul>
-	</li> -->
-	
-<li><a href="view_plan.php"><i class="entypo-quote"></i><span>Event Planning</span></a></li>
-	 <!--<li id="health_status"><a href="new_health_status.php"><i class="entypo-user-add"></i><span>Health Status</span></a>
 
-		 <li class="" id="planhassubopen"><a href="#" onclick="memberExpand(2)"><i class="entypo-quote"></i><span>Event Planning</span></a>
-
-		<ul id="planExpand">
-			<li class="active">
-				<a href="new_plan.php"><span>Add New Event</span></a></li>
-
-			
-		</ul>
-		-->
-
-	<!-- <li class="" id="overviewhassubopen"><a href="#" onclick="memberExpand(3)"><i class="entypo-box"></i><span>Overview</span></a>
-
-		<ul id="overviewExpand">
-			<li class="active">
-				<a href="over_members_month.php"><span>Members per Month</span></a>
-			</li>
-
-			 <li>
-				<a href="over_members_year.php"><span>Members per Year</span></a>
-			</li> 
-
-			<li>
-				<a href="revenue_month.php"><span>Income per Month</span></a>
-			</li>			
-
-		</ul>-->
-
-	<!-- <li class="" id="routinehassubopen"><a href="#" onclick="memberExpand(4)"><i class="entypo-alert"></i><span>Timetable</span></a>
-
-		<ul id="routineExpand">
-			<li class="active">
-				<a href="addroutine.php"><span>Add to Timetable</span></a>
-			</li>
-
-			<li>
-				<a href="editroutine.php"><span>Edit Timetable</span></a>
-			</li>
-
-			<li>
-				<a href="viewroutine.php"><span>View Timetable</span></a>
-			</li>
-
-		</ul>
-
-	</li> -->
-
-	<li id="messageCenter"><a href="messageCenter.php"><i class="entypo-mail"></i><span>Message Center</span></a></li>
-	
-	<li id="adminprofile"><a href="more-userprofile.php"><i class="entypo-folder"></i><span>Profile</span></a></li>
-
-	<li><a href="logout.php"><i class="entypo-logout"></i><span>Logout</span></a></li>
-
-</ul>	
