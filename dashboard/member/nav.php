f65c93046a647eea0d8e3d0707cae453184f4390
Rokyugen <farhansamilme@gmail.com>
2024-11-15 01:38:18 +0800

    nav.php(member): remove health status, they're now in profile section

--- dashboard/member/nav.php
+++ dashboard/member/nav.php
@@ -15,13 +15,16 @@ $isApproved = $row['hasApproved'];
 
 <ul id="main-menu">
     <?php if ($isApproved == 'No'): ?>
+        <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>	
+	    <li id="adminprofile"><a href="more-userprofile.php"><i class="entypo-folder"></i><span>Profile</span></a></li>	
         <li class="approval-message" style="color: grey; font-size: 24px; text-align: center;">
-            Access Restricted for the time being.
+            Further Access Restricted for the time being.
         </li>
+	
     <?php else: ?>
         <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>
         <li id="paymnt"><a href="payments.php"><i class="entypo-star"></i><span>Payments</span></a></li>
-        <li id="health_status"><a href="new_health_status.php"><i class="entypo-user-add"></i><span>Health Status</span></a></li>
+        <!-- <li id="health_status"><a href="new_health_status.php"><i class="entypo-user-add"></i><span>Health Status</span></a></li> -->
 		<li><a href="view_plan.php"><i class="entypo-quote"></i><span>Event Planning</span></a></li>
         <!-- <li id="planhassubopen">
             <a href="#" onclick="memberExpand(2)"><i class="entypo-quote"></i><span>Planning</span></a>
