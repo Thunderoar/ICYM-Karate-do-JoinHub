7d230617d8706f4be4567b0b1ca3507bb652a3f8
Rokyugen <farhansamilme@gmail.com>
2024-11-17 13:38:11 +0800

    plandetail.php(admin): remove showing validity

--- dashboard/admin/plandetail.php
+++ dashboard/admin/plandetail.php
@@ -9,9 +9,6 @@ if($res){
 	echo "<tr>
 		<td height='35'>AMOUNT:</td>
 		<td height='35'>&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input id='boxx' type='text' value='RM".$row['amount']."' readonly></td></tr>
-		<tr>
-		<td height='35'>VALIDITY:</td>
-		<td height='35'>&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input type='text' id='boxx' value='".$row['validity']."' readonly></td>
 		</tr>
 	";
 }
